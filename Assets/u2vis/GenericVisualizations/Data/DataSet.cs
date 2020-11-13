aca661e1b78e247077196657f2e9f5a1a03113bd
preip <p.reipschlaeger@googlemail.com>
2020-11-13 21:44:29 +0100

    Implemented missing methods of the DataSetEnumerator and added comments to DataSet and DataSetEnumerator

--- Assets/u2vis/GenericVisualizations/Data/DataSet.cs
+++ Assets/u2vis/GenericVisualizations/Data/DataSet.cs
@@ -6,9 +6,15 @@ using UnityEngine;
 
 namespace u2vis
 {
+    /// <summary>
+    /// class that represents a specific data set, containing a number of DataDimensions of verious types.
+    /// </summary>
     public class DataSet : IEnumerable<DataDimension>
     {
         #region Private Fields
+        /// <summary>
+        /// The list of data dimensions of this data set.
+        /// </summary>
         private List<DataDimension> _dimensions;
         #endregion
 
@@ -30,11 +36,17 @@ namespace u2vis
         #endregion
 
         #region Constructor
+        /// <summary>
+        /// Creates a new instance of the DataSet class.
+        /// </summary>
         public DataSet()
         {
             _dimensions = new List<DataDimension>();
         }
-
+        /// <summary>
+        /// Creates a new instance of the DataSet class.
+        /// </summary>
+        /// <param name="dimensions">An array of DataDimesions this dataset consists of.</param>
         public DataSet(params DataDimension[] dimensions)
         {
             _dimensions = new List<DataDimension>(dimensions);
@@ -42,8 +54,13 @@ namespace u2vis
         #endregion
 
         #region Private Methods
+        /// <summary>
+        /// Gets the number of Items stored in the dataset.
+        /// </summary>
+        /// <returns></returns>
         private int GetNumberOfItems()
         {
+            // just take the length of the first dimension, assuming that all dimensions are of equal length
             if (_dimensions.Count > 1)
                 return _dimensions[0].Count;
             return 0;
@@ -51,6 +68,10 @@ namespace u2vis
         #endregion
 
         #region Public Methods
+        /// <summary>
+        /// Add a new data dimension to this data set.
+        /// </summary>
+        /// <param name="dimension">The data dimension that should be added.</param>
         public void Add(DataDimension dimension)
         {
             if (_dimensions.Contains(dimension))
@@ -90,11 +111,18 @@ namespace u2vis
         #endregion
 
         #region IEnumarable
+        /// <summary>
+        /// Get a new Enumerator for this data set.
+        /// </summary>
+        /// <returns>the new Enumerator.</returns>
         public IEnumerator<DataDimension> GetEnumerator()
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// Get a new Enumerator for this data set.
+        /// </summary>
+        /// <returns>The new Enumerator.</returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
@@ -102,29 +130,49 @@ namespace u2vis
 
         public class DataSetEnumerator : IEnumerator<DataDimension>
         {
+            /// <summary>
+            /// The data set that is traversed by this enumerator.
+            /// </summary>
             private DataSet _dataSet;
+            /// <summary>
+            /// Index of the current data dimension.
+            /// </summary>
             private int _pos = -1;
-
-            public DataDimension Current => throw new NotImplementedException();
-
-            object IEnumerator.Current => throw new NotImplementedException();
-
+            /// <summary>
+            /// Get the current DataDimension of the Enumerator.
+            /// </summary>
+            public DataDimension Current => _dataSet[_pos];
+            /// <summary>
+            /// Get the current DataDimension of the Enumerator.
+            /// </summary>
+            object IEnumerator.Current => _dataSet[_pos];
+            /// <summary>
+            /// Creates a new instace of the DataSetEnumerator class.
+            /// </summary>
+            /// <param name="dataSet"></param>
             DataSetEnumerator(DataSet dataSet)
             {
                 _dataSet = dataSet;
             }
-
+            /// <summary>
+            /// Dispose this DataSetEnumerator.
+            /// </summary>
             public void Dispose()
             {
                 _dataSet = null;
             }
-
+            /// <summary>
+            /// Move to the next index of the data set.
+            /// </summary>
+            /// <returns></returns>
             public bool MoveNext()
             {
                 _pos++;
                 return _pos < _dataSet.Count;
             }
-
+            /// <summary>
+            /// Reset the enumerator.
+            /// </summary>
             public void Reset()
             {
                 _pos = -1;
