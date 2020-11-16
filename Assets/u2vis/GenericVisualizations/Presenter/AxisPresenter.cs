d30ecf9609f8e3029b6ff930e636b8ca54b5a611
preip <p.reipschlaeger@googlemail.com>
2020-11-16 20:11:54 +0100

    Commented axis and data presenters

--- Assets/u2vis/GenericVisualizations/Presenter/AxisPresenter.cs
+++ Assets/u2vis/GenericVisualizations/Presenter/AxisPresenter.cs
@@ -4,60 +4,128 @@ using UnityEngine;
 
 namespace u2vis
 {
+    /// <summary>
+    /// Class that provides information that can be uses to construct an axis for a visualization.
+    /// </summary>
     [Serializable]
     public class AxisPresenter
     {
         #region Protected Fields
+        /// <summary>
+        /// The caption of the axis. Default is 'none'.
+        /// </summary>
         [SerializeField]
         protected string _caption = "none";
+        /// <summary>
+        /// The orientation of the labels of this axis. Default is Parallel.
+        /// </summary>
         [SerializeField]
         protected LabelOrientation _labelOrientation = LabelOrientation.Parallel;
+        /// <summary>
+        /// Indicates if tha axis is categorical (instead of numerical). Default is false.
+        /// </summary>
         [SerializeField]
         protected bool _isCategorical = false;
+        /// <summary>
+        /// Determines the intervall between 0 and 1 between axis ticks. Default is 0.25. Unused for categorical dimensions.
+        /// </summary>
         [SerializeField]
         protected float _tickIntervall = 0.25f;
+        /// <summary>
+        /// Determines the intervall at which labels will be shown on axis ticks. 1 means every tick, 2 means every second tick and so on. Default is 1.
+        /// </summary>
         [SerializeField]
         protected int _labelTickIntervall = 1;
+        /// <summary>
+        /// Determines the number of decimal places shown for each axis label. Default is 2. Unused for Categorical dimensions.
+        /// </summary>
         [SerializeField]
         protected int _decimalPlaces = 2;
         #endregion
 
         #region Pulic Properties
+        /// <summary>
+        /// Gets or sets the caption for this axis.
+        /// </summary>
         public string Caption
         {
             get { return _caption; }
             set { _caption = value; }
         }
+        /// <summary>
+        /// Gets or sets the orientation of labels for this axis.
+        /// </summary>
         public LabelOrientation LabelOrientation
         {
             get { return _labelOrientation; }
             set { _labelOrientation = value; }
         }
+        /// <summary>
+        /// Indicates if tha axis is categorical (instead of numerical).
+        /// </summary>
         public bool IsCategorical
         {
             get { return _isCategorical; }
             set { _isCategorical = value; }
         }
+        /// <summary>
+        /// Gets or sets the intervall between 0 and 1 between axis ticks. Unused for categorical dimensions.
+        /// </summary>
         public float TickIntervall
         {
             get { return _tickIntervall; }
             set { _tickIntervall = value; }
         }
+        /// <summary>
+        /// Gets or sets the intervall at which labels will be shown on axis ticks. 1 means every tick, 2 means every second tick and so on.
+        /// </summary>
         public int LabelTickIntervall
         {
             get { return _labelTickIntervall; }
             set { _labelTickIntervall = value; }
         }
+        /// <summary>
+        /// Gets or sets the number of decimal places shown for each axis label. Unused for Categorical dimensions.
+        /// </summary>
+        public int DecimalPlaces
+        {
+            get { return _decimalPlaces; }
+            set { _decimalPlaces = value; }
+        }
         #endregion
 
         #region Constructors
+        /// <summary>
+        /// Creates a new instace of the AxisPresenter class.
+        /// </summary>
         public AxisPresenter()
         {
         }
-
+        /// <summary>
+        /// Creates a new instance of the AxisPresenter class.
+        /// </summary>
+        /// <param name="caption">The caption of this axis.</param>
         public AxisPresenter(string caption)
         {
             _caption = caption;
+        }
+        /// <summary>
+        /// Creates a new instance of the AxisPresenter class.
+        /// </summary>
+        /// <param name="cpation">The caption of this axis.</param>
+        /// <param name="labelOrientation">The orientation of labels for this axis.</param>
+        /// <param name="isCategorical">Indicates if this axis consists of categorical values (instead of numerical ones).</param>
+        /// <param name="tickintevall">The intervall between 0 and 1 between ticks of this axis. Unused for categorical values.</param>
+        /// <param name="labelTickIntervall">The intervall where labels are shown a the axis ticks. 1 means every tick, 2 means every second tick, and so on.</param>
+        /// <param name="decimalPlaces">The number of decimal places shown for the labels of this axis. Unused for categorical values.</param>
+        public AxisPresenter(string cpation, LabelOrientation labelOrientation, bool isCategorical, float tickintevall, int labelTickIntervall, int decimalPlaces)
+        {
+            _caption = Caption;
+            _labelOrientation = LabelOrientation;
+            _isCategorical = isCategorical;
+            _tickIntervall = tickintevall;
+            _labelTickIntervall = labelTickIntervall;
+            _decimalPlaces = decimalPlaces;
         }
         #endregion
 
@@ -146,6 +214,13 @@ namespace u2vis
         #endregion
 
         #region Protected Methods
+        /// <summary>
+        /// Generates the ticks and labels for a numerical axis based on the specified parameters.
+        /// </summary>
+        /// <param name="min">The minimum numerical value for the axis labels.</param>
+        /// <param name="max">The maximum numerical value for the axis labels.</param>
+        /// <param name="tickCount">The number of ticks the axis should have.</param>
+        /// <returns></returns>
         protected virtual AxisTick[] GenerateNumerical(float min, float max, int tickCount)
         {
             var ticks = new AxisTick[tickCount];
@@ -165,7 +240,13 @@ namespace u2vis
             }
             return ticks;
         }
-
+        /// <summary>
+        /// Generates the ticks and labels for a categorical axis based on the specified values.
+        /// </summary>
+        /// <param name="dim">The data dimesion for which the axis ticks should be generated.</param>
+        /// <param name="start">The start index for the axis ticks.</param>
+        /// <param name="end">The end index for the axis ticks.</param>
+        /// <returns></returns>
         protected virtual AxisTick[] GenerateCategorical(DataDimension dim, int start, int end)
         {
             int length = end - start;
--- Assets/u2vis/GenericVisualizations/Presenter/GenericDataPresenter.cs
+++ Assets/u2vis/GenericVisualizations/Presenter/GenericDataPresenter.cs
@@ -3,16 +3,36 @@ using UnityEngine;
 
 namespace u2vis
 {
+    /// <summary>
+    /// Delegate which indicates that the highlight at a speicifc index of a DataPresenter has changed.
+    /// </summary>
+    /// <param name="sender">The sender which raised this event.</param>
+    /// <param name="itemIndex">The index which has changed.</param>
     public delegate void HighlightChangedHandler(GenericDataPresenter sender, int itemIndex);
+    /// <summary>
+    /// Class that represents a generic data presenter for all kinds of visualizations that support one axis per dimension (like 2D bar charts and scatterplots).
+    /// </summary>
     public class GenericDataPresenter : MonoBehaviour
     {
+        /// <summary>
+        /// The default index of a data dimension within the data set. Used for the dimension indices array.
+        /// </summary>
         public const int DEFAULT_INDEX = 0;
 
         #region Protected Fields
+        /// <summary>
+        /// The DataProvider which provides the DataSet used ba this DataPresenter.
+        /// </summary>
         [SerializeField]
         protected AbstractDataProvider _dataProvider = null;
+        /// <summary>
+        /// The currently selected minimum item index within the data set. Default is 0.
+        /// </summary>
         [SerializeField]
         protected int _selectedMinItem = 0;
+        /// <summary>
+        /// The currently selected maximum item index within the data set. Default is 0.
+        /// </summary>
         [SerializeField]
         protected int _selectedMaxItem = 0;
         // Holds the data dimensions of this presenter as a list of indices to the data dimensions of the data provider.
@@ -25,40 +45,81 @@ namespace u2vis
         //Represented the reference of Axis which also is showed in the editor script
         [SerializeField]
         protected AxisPresenter[] _axisPresenters = null;
+        /// <summary>
+        /// Indicates if this DataPresenter has ben initialized or not.
+        /// </summary>
         protected bool _isInitialized = false;
         #endregion
 
         #region Public Properties
+        /// <summary>
+        /// Gets the DataProvider which provides the DataSet used by this DataPresenter.
+        /// </summary>
         public AbstractDataProvider DataProvider => _dataProvider;
         /// <summary>
         /// Returns a list of all data dimensions.
         /// WARNING: Not very efficient, use the index property instead.
         /// </summary>
         public DataDimension[] Dimensions => GetDimensionsArray();
+        /// <summary>
+        /// Gets the number of data dimensions provided by this DataPresenter.
+        /// </summary>
         public int NumberOfDimensions => _dataDimensionsIndices.Length;
+        /// <summary>
+        /// Gets the total number of items contained within the DataSet used by this DataPresenter.
+        /// </summary>
         public int TotalItemCount => NumberOfDimensions == 0 ? 0 : this[0].Count;
+        /// <summary>
+        /// Gets the number of items currently selected.
+        /// </summary>
         public int SelectedItemsCount => _selectedMaxItem - _selectedMinItem;
+        /// <summary>
+        /// Gets or sets the minimum index of the currently selected items.
+        /// If the value is large than SelectedMaxIndex, both values are switched.
+        /// </summary>
         public int SelectedMinItem
         {
             get { return _selectedMinItem; }
             set { SetSelectedItemIndices(value, _selectedMaxItem); }
         }
+        /// <summary>
+        /// Gets or sets the maximum index if the currently selected items.
+        /// If the value is lower than SelectedMinIndex, both values are switched.
+        /// </summary>
         public int SelectedMaxItem
         {
             get { return _selectedMaxItem; }
             set { SetSelectedItemIndices(_selectedMinItem, value); }
         }
+        /// <summary>
+        /// Gets the DataDimension at the specified index out of the dimensions this DataPresenter provides.
+        /// Note: This is not the index within the DataSet, but within the DataPresenter.
+        /// </summary>
+        /// <param name="index">The index for which the DataDimension should be gotten.</param>
+        /// <returns>the reulting DataDimension.</returns>
         public DataDimension this[int index] => _dataProvider.Data[_dataDimensionsIndices[index]];
+        /// <summary>
+        /// Gets the List of AxisPresenters defined for this DataPresenter.
+        /// </summary>
         // TODO: return ReadOnly list instead of real array
         public AxisPresenter[] AxisPresenters => _axisPresenters;
         #endregion
 
         #region Events
+        /// <summary>
+        /// Raised whenever the data of the DataPresenter was updates, e.g., when the selected indices have changed or the underlying DataSet was updated.
+        /// </summary>
         public event EventHandler<DataPresenterEventArgs> DataUpdated;
+        /// <summary>
+        /// Raised whenever the highlight of a specific index has changed.
+        /// </summary>
         public event HighlightChangedHandler HighlightChanged;
         #endregion
 
         #region Constructors
+        /// <summary>
+        /// Creates a new instance of the GenericDataPresenter class.
+        /// </summary>
         protected GenericDataPresenter()
         {
             _dataDimensionsIndices = new int[1];
@@ -68,16 +129,24 @@ namespace u2vis
         #endregion
 
         #region Protected Methods
+        /// <summary>
+        /// Called by unity once at the start of this script.
+        /// </summary>
         protected virtual void Start()
         {
-            // in case the provider was set in the editor
+            // in case the provider was set in the editor, the event handlers and non editor variables have to be initialized seperately
             if (!_isInitialized)
             {
                 _dataProvider.DataChanged += Provider_DataUpdated;
                 _highlightedItems = new bool[TotalItemCount];
             }
         }
-
+        /// <summary>
+        /// Generates an array of DataDimensions that this DataPresenter provides.
+        /// Since the provider only stores indices to the DataSet instead of actual references,
+        /// this list needs to be generated by accessing the DataSet.
+        /// </summary>
+        /// <returns>The resulting array of DataDimensions.</returns>
         protected virtual DataDimension[] GetDimensionsArray()
         {
             var d = new DataDimension[_dataDimensionsIndices.Length];
@@ -85,7 +154,9 @@ namespace u2vis
                 d[i] = _dataProvider.Data[_dataDimensionsIndices[i]];
             return d;
         }
-
+        /// <summary>
+        /// Sets the axis presenters with initial values, assuming each data dimension has its own axis.
+        /// </summary>
         protected virtual void SetupInitialAxisPresenters()
         {
             if (_dataProvider == null)
@@ -94,7 +165,11 @@ namespace u2vis
             for (int i = 0; i < NumberOfDimensions; i++)
                 _axisPresenters[i] = new AxisPresenter(this[i].Name);
         }
-
+        /// <summary>
+        /// Called whenever the data of the DataProvider has changed.
+        /// </summary>
+        /// <param name="sender">The DataProvider which data has changed.</param>
+        /// <param name="e">The event arguments.</param>
         protected virtual void Provider_DataUpdated(object sender, EventArgs e)
         {
             if (_highlightedItems.Length != _dataProvider.Data.NumOfItems)
@@ -105,6 +180,14 @@ namespace u2vis
         #endregion
 
         #region Public Methods
+        /// <summary>
+        /// Initialize this DataPresenter and generate axis descriptions, etc.
+        /// Can be called multiple times to reset the DataPresenter.
+        /// </summary>
+        /// <param name="provider">The DataProvider that provides the DataSet used for this DataPresenter.</param>
+        /// <param name="minIndex">The minimum index of the items within the DataSet which this DataPresenter represents.</param>
+        /// <param name="maxIndex">The maximum index of the items within the DataSet which this DataPresenter represents.</param>
+        /// <param name="dimensionIndices">A list of indices of the DataDimensions within the DataSet which this DataPresenter should represent.</param>
         public virtual void Initialize(IDataProvider provider, int minIndex, int maxIndex, params int[] dimensionIndices)
         {
             if (provider == null)
@@ -128,7 +211,12 @@ namespace u2vis
             var e = new DataPresenterEventArgs();
             DataUpdated?.Invoke(this, e);
         }
-
+        /// <summary>
+        /// Set the minimum and maximum selected indices of the DataSet this DataPresneter represents.
+        /// Note: If minimum and maximum are switched if one is larger/lower than the other.
+        /// </summary>
+        /// <param name="min">The new minimum selected index.</param>
+        /// <param name="max">The new maximum selected index.</param>
         public virtual void SetSelectedItemIndices(int min, int max)
         {
             int numOfItems = _dataProvider.Data.NumOfItems;
@@ -148,13 +236,19 @@ namespace u2vis
             _selectedMaxItem = max;
             DataUpdated?.Invoke(this, e);
         }
-
+        /// <summary>
+        /// Reset the axis properties to their default values.
+        /// </summary>
         public virtual void ResetAxisProperties()
         {
             SetupInitialAxisPresenters();
         }
 
         #region Highlights
+        /// <summary>
+        /// Toggles the highlight flag for the specified index.
+        /// </summary>
+        /// <param name="index">The index for which the highlight should be toggled.</param>
         public virtual void ToogleItemHighlight(int index)
         {
             _highlightedItems[index] = !_highlightedItems[index];
@@ -162,14 +256,20 @@ namespace u2vis
             if (index >= _selectedMinItem || index < _selectedMaxItem)
                 DataUpdated?.Invoke(this, new DataPresenterEventArgs());
         }
-
+        /// <summary>
+        /// Indicates if the item at the specified index is currently highlightet or not.
+        /// </summary>
+        /// <param name="index">The index for which the highlight state should be checked.</param>
+        /// <returns>true if the item is currently highlighted, otherwise false.</returns>
         public bool IsItemHighlighted(int index)
         {
             if (index < 0 || index >= _highlightedItems.Length)
                 return false;
             return _highlightedItems[index];
         }
-
+        /// <summary>
+        /// Clear the highlight flags for all indices.
+        /// </summary>
         public void ClearHighlights()
         {
             for (int i = 0; i < _highlightedItems.Length; i++)
