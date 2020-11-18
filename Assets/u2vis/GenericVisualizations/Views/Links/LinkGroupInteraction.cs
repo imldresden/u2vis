3b8f48e92b58f3d89ed8761d74d8f242ea37c2d4
preip <p.reipschlaeger@googlemail.com>
2020-11-18 21:42:24 +0100

    Commented LinkGroups and GenericVisualizationStyle, also added methods to create them from code

--- Assets/u2vis/GenericVisualizations/Views/Links/LinkGroupInteraction.cs
+++ Assets/u2vis/GenericVisualizations/Views/Links/LinkGroupInteraction.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 namespace u2vis
 {
+    /// <summary>
+    /// Interaction component for link groups which can be used a an example.
+    /// Disables all current highlights whenever a double tap is performed.
+    /// </summary>
     public class LinkGroupInteraction : UiElement
     {
+        #region
+        /// <summary>
+        /// The link group which this interaction script handles.
+        /// </summary>
         [SerializeField]
         protected LinkGroup _linkGroup = null;
+        /// <summary>
+        /// The time intervall used to determine when a touble tap has been performed.
+        /// </summary>
         [SerializeField]
         protected float _doubleTapIntervall = 0.5f;
-        
+        /// <summary>
+        /// Timer variable which is set after a tap was performed to determine if the next tap stays within the double tap intervall.
+        /// </summary>
         protected float _doubleTapTimer = -1;
+        #endregion
 
+        #region Public Properties
+        public LinkGroup Lingroup
+        {
+            get { return _linkGroup; }
+            set { _linkGroup = value; }
+        }
+        #endregion
+
+        #region Protected Methods
+        /// <summary>
+        /// Called by Unity every frame.
+        /// </summary>
         protected void Update()
         {
             if (_doubleTapTimer > 0)
@@ -22,7 +48,12 @@ namespace u2vis
                     _doubleTapTimer = -1;
             }
         }
-
+        /// <summary>
+        /// Called whenever a touch collided with the collider this script is attached to.
+        /// </summary>
+        /// <param name="finger">The finger object representing the touch.</param>
+        /// <param name="order">The order in which this collider was hit.</param>
+        /// <param name="hit">The raycast hit which was used to determine the hit.</param>
         public override void OnFingerDown(LeanFinger finger, int order, RaycastHit hit)
         {
             base.OnFingerUp(finger, order, hit);
@@ -39,11 +70,14 @@ namespace u2vis
                 _doubleTapTimer = -1;
             }
         }
-
+        /// <summary>
+        /// Clear the links of thi
+        /// </summary>
         protected virtual void ClearLinks()
         {
             if (_linkGroup != null)
-                _linkGroup.Clear();
+                _linkGroup.ClearHighlights();
         }
+        #endregion
     }
 }
